e5153ac671b0b42858caab3cb396e4f87db65bef
Omar Hawas <omarhawas94@gmail.com>
2024-09-18 13:52:55 -0400

    initial commit. pushing User devise model

---
+++ app/controllers/chatly_devise_controller.rb
@@ -0,0 +1,18 @@
+class ChatlyDeviseController < ApplicationController
+    class Responder < ApplicationController::Responder
+        def to_chatly_stream
+            controller.render(options.merge(formats: :html))
+        rescue ActionView::MissingTemplate => e
+            if get?
+                raise e
+            elsif has_errors? && default_action
+                render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+            else
+                redirect_to navigation_location
+            end
+        end
+    end
+
+    self.responder = Responder
+    respond_to :html, :chatly_stream
+end
